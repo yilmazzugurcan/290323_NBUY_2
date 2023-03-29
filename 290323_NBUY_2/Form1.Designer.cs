771c31f29247db1ca7c443ef05272631a00afc4c
yilma <yilma@DESKTOP-U1LH654>
2023-03-29 21:32:08 +0300

    Add project files.

---
+++ 290323_NBUY_2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace _290323_NBUY_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbMarka = new System.Windows.Forms.ComboBox();
+            this.cmbmodel = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbMarka
+            // 
+            this.cmbMarka.FormattingEnabled = true;
+            this.cmbMarka.Items.AddRange(new object[] {
+            "VOLKSWAGEN",
+            "OPEL"});
+            this.cmbMarka.Location = new System.Drawing.Point(92, 74);
+            this.cmbMarka.Name = "cmbMarka";
+            this.cmbMarka.Size = new System.Drawing.Size(160, 21);
+            this.cmbMarka.TabIndex = 0;
+            this.cmbMarka.SelectedIndexChanged += new System.EventHandler(this.cmbMarka_SelectedIndexChanged);
+            // 
+            // cmbmodel
+            // 
+            this.cmbmodel.FormattingEnabled = true;
+            this.cmbmodel.Location = new System.Drawing.Point(288, 74);
+            this.cmbmodel.Name = "cmbmodel";
+            this.cmbmodel.Size = new System.Drawing.Size(162, 21);
+            this.cmbmodel.TabIndex = 1;
+            this.cmbmodel.SelectedIndexChanged += new System.EventHandler(this.cmbmodel_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(92, 201);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(358, 161);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Renk Değiştir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(596, 129);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(133, 46);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmbmodel);
+            this.Controls.Add(this.cmbMarka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMarka;
+        private System.Windows.Forms.ComboBox cmbmodel;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ 290323_NBUY_2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _290323_NBUY_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("Marka", 100);
+            listView1.Columns.Add("Model", 100);
+        }
+
+        private void cmbmodel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog Renk = new ColorDialog();
+            Renk.ShowDialog();
+            this.BackColor = Renk.Color;
+        }
+
+        private void cmbMarka_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string secilenMarka = cmbMarka.SelectedItem.ToString();
+            
+
+            if (secilenMarka == "VOLKSWAGEN")
+            {
+                cmbmodel.Items.Add("POLO");
+                cmbmodel.Items.Add("GOLF");
+            }
+            else if (secilenMarka == "OPEL")
+            {
+                cmbmodel.Items.Add("INSIGNIA");
+                cmbmodel.Items.Add("ASTRA");
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            
+            string selectedMarka = cmbMarka.SelectedItem.ToString();
+            string selectedModel = cmbmodel.SelectedItem.ToString();
+            ListViewItem item = new ListViewItem(new[] { selectedMarka, selectedModel });
+            listView1.Items.Add(item);
+        }
+    }
+}
